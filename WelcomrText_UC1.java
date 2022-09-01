05ceb6b4484d5cb42941c714d1be2bfc2c2b12c0
gitlinux12 <mshirole94@gmail.com>
2022-09-01 23:44:29 +0530

    CheckAttendence

--- WelcomrText_UC1.java
+++ WelcomrText_UC1.java
@@ -1,5 +1,22 @@
 package OOP_Employee_Edge;
 
 public class WelcomrText_UC1 {
+	int IS_FULL_TIME = 1;
+	
+	public void getAttendance() {
+
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+		   System.out.println("Employee is present");
+		else
+		   System.out.println("Employee is absent");
+		}
+
+
+	public static void main(String[] args) {
+		WelcomrText_UC1 emp = new WelcomrText_UC1();
+		emp.getAttendance();
+
+	}
 
 }
