52125d253695c6662f049ba06734b9391a39594b
gitlinux12 <mshirole94@gmail.com>
2022-09-03 16:29:30 +0530

    Employee Wage For Month

--- WelcomrText_UC1.java
+++ WelcomrText_UC1.java
@@ -6,16 +6,24 @@ public class WelcomrText_UC1 {
 	public static double WAGE_PER_HOUR = 20;
 	public static double FULL_DAY_HOUR = 8;
 	public static double PART_TIME_HOUR = 4;
+	public static int WORKING_DAYS = 20;
+	
+	public static double monthWages = 0;
 	
 	public static int attendance() {
+		double wage = 0;
 		switch ((int) Math.floor(Math.random() * 10) % 3) {
 		case IS_PRESENT:
 			System.out.println("Emplyee is present.");
-			System.out.println("Employee daily wage is "+(FULL_DAY_HOUR * WAGE_PER_HOUR));
+			wage = FULL_DAY_HOUR * WAGE_PER_HOUR;
+			System.out.println("Employee daily wage is "+wage);
+			monthWages += wage;
 			return IS_PRESENT;
 		case IS_PART_TIME:
 			System.out.println("Emplyee is part time.");
-			System.out.println("Employee daily wage is "+(PART_TIME_HOUR * WAGE_PER_HOUR));
+			wage = PART_TIME_HOUR * WAGE_PER_HOUR;
+			System.out.println("Employee daily wage is "+wage);
+			monthWages += wage;
 			return IS_PART_TIME;
 		default:
 			System.out.println("Employee is absenet.");
@@ -24,9 +32,16 @@ public class WelcomrText_UC1 {
 		}
 	}
 	
+	public static double monthlyWage() {
+		for (int day = 1; day <= 20; day++) {
+			attendance();
+		}
+		return monthWages;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		attendance();
+		System.out.println("Monthly wage for employee is "+monthlyWage());
 	}
 
 }
